ab36b57561a239e00e604840c1fdc380f217cd8e
achitchen <61246622+achitchen@users.noreply.github.com>
2023-02-13 21:55:44 +0000

    Tweaked launch charge values

--- Pit Warrior/Assets/Scripts/PlayerMovement.cs
+++ Pit Warrior/Assets/Scripts/PlayerMovement.cs
@@ -70,7 +70,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (isChargingLaunch && launchCharge <= maxLaunchCharge)
         launchCharge++;
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.05f);
         StartCoroutine(addLaunchCharge());
     }
 
