f1fc8925fc75bc48c8ce45ea1658ac02b0006996
achitchen <61246622+achitchen@users.noreply.github.com>
2023-02-14 21:28:41 +0000

    Added socket/potting mechanic

---
+++ Pit Warrior/Assets/EnemyPotted.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPotted : MonoBehaviour
+{
+    private SpriteRenderer socketRenderer;
+    private SpriteRenderer enemyRenderer;
+    private GameObject socket;
+    private GameObject filledSocket;
+    [SerializeField] bool isFilled;
+
+    private void Start()
+    {
+        socket = transform.Find("Socket").gameObject;
+        socket.SetActive(true);
+        filledSocket = transform.Find("FilledSocket").gameObject;
+        filledSocket.SetActive(false);
+        socketRenderer = filledSocket.transform.Find("Body").gameObject.GetComponent<SpriteRenderer>();
+        isFilled = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" && !isFilled)
+        {
+            enemyRenderer = collision.transform.Find("Body").gameObject.GetComponent<SpriteRenderer>();
+            socket.SetActive(false);
+            filledSocket.SetActive(true);
+            socketRenderer.color = enemyRenderer.color;
+            collision.gameObject.SetActive(false);
+            isFilled = true;
+        }
+    }
+}
