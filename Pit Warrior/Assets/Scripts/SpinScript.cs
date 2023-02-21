3f54f23716b8b6f3124c6c2d95824e1b4ca8e8a8
achitchen <61246622+achitchen@users.noreply.github.com>
2023-02-21 21:11:07 +0000

    Started collision behaviour

--- Pit Warrior/Assets/Scripts/SpinScript.cs
+++ Pit Warrior/Assets/Scripts/SpinScript.cs
@@ -4,11 +4,44 @@ using UnityEngine;
 
 public class SpinScript : MonoBehaviour
 {
-    [SerializeField] int spinSpeed;
+    [SerializeField] int spinSpeed = 30;
+    [SerializeField] int hitDelay = 1;
+    [SerializeField] float impactForce = 5;
+    public bool isHit;
+    private Vector2 impactDirection;
+    private Rigidbody2D enemyRb;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        isHit = false;
+        if (enemyRb == null)
+        {
+            enemyRb = GetComponent<Rigidbody2D>();
+        }
+    }
+
     void Update()
     {
-        transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime);
+        if (!isHit)
+        {
+            transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision tag" + collision.gameObject.tag.ToString());
+        if (collision.gameObject.tag == "PlayerAttack")
+        { 
+            isHit = true;
+            StartCoroutine(ResumeMovement());
+        }
+    }
+
+    private IEnumerator ResumeMovement()
+    {
+        //enemyRb.AddForce(impactDirection * impactForce, ForceMode2D.Force);
+        yield return new WaitForSeconds(hitDelay);
+        isHit = false;
     }
 }
